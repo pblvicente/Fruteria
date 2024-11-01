1af5637d16483b6e7a5695ab51e63bffb667b045
Pablo Vicente <pvicentebusiness@gmail.com>
2024-11-01 02:49:56 +0100

    [ADD] Utility functions (getDate, createElementWithClassNames, createEmptyMessageElement) | [MOD] Extracted functionalities from createFruitElements to other functions (giveFunctionalityToClickOnFruitImage, createProductPanelElement)

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,11 @@
 const host = "http://localhost:3000";
-var fruitArray = [];
-var shoppingCartArray = [];
+let fruitArray = [];
+let shoppingCartArray = {
+  date: "",
+  products: []
+};
 
-window.addEventListener("load", startApplication());
+window.addEventListener("load", () => startApplication());
 
 function startApplication() {
   getFruitsArrayFromAPI().then((fruits) => {
@@ -12,7 +15,7 @@ function startApplication() {
       a.name.toLocaleLowerCase().localeCompare(b.name.toLocaleLowerCase())
     );
     createFruitElements(fruitArray);
-    giveShoppingCartButton();
+    giveFunctionalityToShoppingCartButton();
   });
 }
 
@@ -42,78 +45,36 @@ function createFruitElements(fruitArray) {
   let fruitContainer = document.getElementById("fruitContainer");
 
   fruitArray.forEach((fruit) => {
-    let column = document.createElement("div");
-    column.classList.add("col");
+    let column = createElementWithClassNames("div", "col");
 
-    let card = document.createElement("div");
-    card.classList.add("card");
+    let card = createElementWithClassNames("div", "card");
 
-    let img = document.createElement("img");
-    img.classList.add(
-      "card-img-top",
-      "ratio",
-      "ratio-1x1",
-      "bg-secondary-subtle"
-    );
+    let img = createElementWithClassNames("img", ["card-img-top", "ratio", "ratio-1x1", "bg-secondary-subtle"]);
     img.src = "./media/fruits/" + fruit.mask + ".jpg";
-    img.alt = fruit.mask;
-    img.addEventListener("error", (event) => {
-      img.src = null;
-      img.src = "./media/default/image.jpg";
-    });
+    img.alt = fruit.mask;    
 
-    img.addEventListener("click", (event) => {
-      if (shoppingCartArray.length <= 0) {
-        document.getElementById("shoppingCartMessage").innerHTML = "Vacío";
-      }
-    
-      let target = shoppingCartArray.findIndex(
-        (element) => element.mask == img.alt
-      );
-    
-      if (target == -1) {
-        shoppingCartArray.push({
-          mask: fruit.mask,
-          name: fruit.name,
-          price: fruit.price,
-          quantity: 1,
-        });
-      } else {
-        shoppingCartArray[target].quantity =
-        shoppingCartArray[target].quantity + 1;
-      }
-    });
+    let cardBody = createElementWithClassNames("div", "card-body");
 
-    let cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+    let rowFruitInfo = createElementWithClassNames("div", ["row", "gy-1", "justify-content-between", "card-text"]);
 
-    let rowFruitInfo = document.createElement("div");
-    rowFruitInfo.classList.add(
-      "row",
-      "gy-1",
-      "justify-content-between",
-      "card-text"
-    );
-
-    let colFruitName = document.createElement("div");
-    colFruitName.classList.add("col", "text-truncate");
+    let colFruitName = createElementWithClassNames("div", ["col", "text-truncate"]);
     colFruitName.innerHTML = fruit.name;
 
-    let colFruitPrice = document.createElement("div");
-    colFruitPrice.classList.add("col-12", "col-md-auto");
+    let colFruitPrice = createElementWithClassNames("div", ["col-12", "col-md-auto"]);
     colFruitPrice.innerHTML = fruit.price + "&#8364";
 
-    let colFruitInputContainer = document.createElement("div");
-    colFruitInputContainer.classList.add("col-12");
+    let colFruitInputContainer = createElementWithClassNames("div", "col-12");
 
-    let colFruitInput = document.createElement("input");
+    let colFruitInput = createElementWithClassNames("input", "form-control");
     colFruitInput.type = "number";
     colFruitInput.id = fruit.mask;
     colFruitInput.name = fruit.mask;
     colFruitInput.value = 0;
     colFruitInput.min = 0;
     colFruitInput.max = 100;
-    colFruitInput.classList.add("form-control");
+
+    img.addEventListener("error", () => { img.src = "./media/default/image.jpg"; });
+    img.addEventListener("click", () => { giveFunctionalityToClickOnFruitImage(fruit, colFruitInput) });
 
     fruitContainer.appendChild(column);
     column.appendChild(card);
@@ -125,13 +86,13 @@ function createFruitElements(fruitArray) {
     rowFruitInfo.appendChild(colFruitInputContainer);
     colFruitInputContainer.appendChild(colFruitInput);
   });
-}  
+}
 
-function giveShoppingCartButton() {
+function giveFunctionalityToShoppingCartButton() {
   let shoppingCartButton = document.getElementById("shoppingCartButton");
   let shoppingCartMessage = document.getElementById("shoppingCartMessage");
 
-  shoppingCartButton.addEventListener("click", (e) => {
+  shoppingCartButton.addEventListener("click", () => {
     if (shoppingCartArray.length >= 1) {
       let message = "";
       let total = 0;
@@ -178,4 +139,92 @@ function giveShoppingCartButton() {
 
     shoppingCartArray = [];
   });
+}
+
+// FUNCTIONALITIES
+
+function giveFunctionalityToClickOnFruitImage(fruit, input) {  
+
+  let numberSelected = parseInt(input.value);
+
+  if(numberSelected <= 0) { 
+    
+    alert("Introduce un número mayor que 0")
+    return;
+  
+  }
+  
+  input.value = 0;
+  let message = document.querySelector("#productPanelMessage h6");
+  if (message) {
+    message.remove();
+    document.getElementById("productPanelMessage").classList.replace("align-self-center", "align-self-start")    
+  }
+
+  let targetIndex = shoppingCartArray.products.findIndex((element) => element.fruitID == fruit.id);
+
+  if (targetIndex == -1) {
+    shoppingCartArray.products.push({
+      fruitID: fruit.id,
+      totalAmount: numberSelected * fruit.price,
+      totalKilos: numberSelected
+    });
+  } else {
+    shoppingCartArray.products[targetIndex].totalKilos = shoppingCartArray.products[targetIndex].totalKilos + numberSelected;
+    shoppingCartArray.products[targetIndex].totalAmount = shoppingCartArray.products[targetIndex].totalKilos * fruit.price;
+  }
+
+  createProductPanelElement(fruit, numberSelected);
+
+}
+
+function createProductPanelElement(fruit, numberSelected) {
+
+  let productPanel = document.getElementById("productPanelMessage");
+  let row = createElementWithClassNames("div", ["row", "bg-dark", "border-2", "border-bottom", "px-2", "align-items-center", "justify-content-center", fruit.mask]);
+  let imgContainer = createElementWithClassNames("div", ["col-auto"]);
+  let img = createElementWithClassNames("img", ["ratio", "ratio-1x1", "py-2", "p-xxl-1", "rounded-circle", "imgCustom"]);
+  img.src = "./media/fruits/" + fruit.mask + ".jpg";
+  img.alt = fruit.mask;
+  let nameContainer = createElementWithClassNames("div", ["col-auto", "text-truncate"]);
+  nameContainer.innerHTML = fruit.name;
+  let numberSelectedContainer = createElementWithClassNames("div", ["col-auto"]);
+  numberSelectedContainer.innerHTML = "x " + numberSelected;
+  
+  productPanel.appendChild(row);
+  row.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  row.appendChild(nameContainer)
+  row.appendChild(numberSelectedContainer);
+
+}
+
+// UTILITIES
+
+function getDate() {
+  let date = new Date();
+  return date.getMonth() + "/" + date.getFullYear();
+}
+
+function createElementWithClassNames(element, classNames) {
+  let div = document.createElement(element);
+  if (typeof classNames === "string") {
+    div.classList.add(classNames);
+  }
+  if (Array.isArray(classNames)) {
+    div.classList.add(...classNames);
+  }
+  return div;
+}
+
+function createEmptyMessageElement(id) {
+
+  let div = document.getElementById(id);
+  div.classList.replace("align-self-start", "align-self-center")
+  let h6 = createElementWithClassNames("h6", "text-center");
+  h6.innerText = "Vacío";
+  div.appendChild(h6);
+
+  return div;
+
 }
